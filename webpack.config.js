bb3b48dd9b8b9e6f14cb07a6316e854229211601
Rajat Kumar <rajatk@netflix.com>
2019-02-08 09:57:24 -0800

    chore: try packtracker

--- webpack.config.js
+++ webpack.config.js
@@ -1,25 +1,31 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+const PacktrackerPlugin = require('@packtracker/webpack-plugin');
 const webpack = require('webpack');
 module.exports = {
-    devtool: 'inline-source-map',
+    devtool: 'source-map',
     performance: {
         hints: 'warning'
     },
     optimization: {
-        splitChunks: {
-            maxSize: 15000,
-            minSize: 10000,
-            name: false,
-            cacheGroups: {
-                vendors: {
-                    test: /[\\/]node_modules[\\/](react|react-dom)[\\/]/,
-                    name: 'react-vendor',
-                    chunks: 'all'
-                }
-            }
+        runtimeChunk: {
+            name: entrypoint => `runtimechunk~${entrypoint.name}`
         }
     },
+    // optimization: {
+    //     splitChunks: {
+    //         maxSize: 15000,
+    //         minSize: 10000,
+    //         name: false,
+    //         cacheGroups: {
+    //             vendors: {
+    //                 test: /[\\/]node_modules[\\/](react|react-dom)[\\/]/,
+    //                 name: 'react-vendor',
+    //                 chunks: 'all'
+    //             }
+    //         }
+    //     }
+    // },
     entry: './src/index.js',
     output: {
         path: path.join(__dirname, 'build'),
@@ -47,6 +53,11 @@ module.exports = {
         new HtmlWebpackPlugin({
             template: path.resolve('./index.html')
         }),
-        new webpack.HotModuleReplacementPlugin()
+        new webpack.HotModuleReplacementPlugin(),
+        new PacktrackerPlugin({
+            project_token: 'e345d9d7-0442-4e09-8a2d-581c5c716266',
+            upload: true,
+            fail_build: true
+        })
     ]
 };
